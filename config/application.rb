e44d240dbede84f4670bd65da3423355716dd73b
jegpub <jegpub@gmail.com>
2017-02-27 21:58:29 +0000

    Images now showing dynamically

--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,12 @@ module Estuk
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.paperclipdefaults = {
+    config.paperclip_defaults = {
       storage: :s3,
       s3_credentials: {
         bucket: ENV['AWS_BUCKET'],
+        s3_region: ENV['AWS_REGION'],
+        s3_host_name: ENV['AWS_REGION'] + '.amazonaws.com',
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
       }
